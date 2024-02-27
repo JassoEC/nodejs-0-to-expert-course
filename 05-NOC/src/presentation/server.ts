b11ca6fe90ba9c569d98e4b6312dff4319ccfca2
Emanuel Campos Jasso <34528183+JassoEC@users.noreply.github.com>
2024-02-27 17:02:12 -0600

    add custom dev server

--- 05-NOC/src/presentation/server.ts
+++ 05-NOC/src/presentation/server.ts
@@ -1,5 +1,19 @@
+import { CheckService } from "../domain/usecases/checks/check-service"
+import { CronService } from "./cron/cron-sevice"
+
 export class Server {
   public static start() {
     console.log('Server started')
+
+    CronService.createJob('*/5 * * * * *', () => {
+      // new CheckService().execute('https://www.google.com')
+
+      const url = 'http://localhost:3000/'
+
+      new CheckService(
+        () => console.log(`Success on check service: ${url}`),
+        (error) => console.error(error)
+      ).execute(url)
+    })
   }
 }
